035664caecba3bcc43bf036ffac40361e0133bc5
Aman Kapur <amankapur91@gmail.com>
2012-06-30 18:24:07 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomshare::Application.config.secret_token = '613a00bbaeb960b29fb47a507b8d4a500f87852acd882657d65340ca339cc21ac8350b12a1a6dd402cfaa6c6dcc0e17785bd4aab4a72ebb4cfa6855bdcfc946f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nomshare::Application.config.session_store :cookie_store, key: '_nomshare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nomshare::Application.config.session_store :active_record_store
